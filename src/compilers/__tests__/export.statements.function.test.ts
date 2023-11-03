7654083bfbaf5a8f9a830418ee8fb2133996bd06
ByungJoon Lee <imjuni@gmail.com>
2023-11-04 02:49:36 +0900

    Add test for issue #81

--- src/compilers/__tests__/export.statements.function.test.ts
+++ src/compilers/__tests__/export.statements.function.test.ts
@@ -58,6 +58,49 @@ export default () => {};
     ]);
   });
 
+  it('one arrow function, export by toLocaleString', async () => {
+    const uuid = randomUUID();
+    const filename = `${uuid}.ts`;
+    const source = `
+export const toLocaleString = (date: Date | number) => new Date(date).toLocaleString();
+    `;
+
+    const sourceFile = context.project.createSourceFile(filename, source.trim());
+
+    const statement = await getExportStatement(
+      sourceFile,
+      {
+        project: context.tsconfig,
+        exportFilename: 'index.ts',
+      },
+      { eol: '\n' },
+    );
+
+    expect(statement).toMatchObject([
+      {
+        path: {
+          filename,
+          dirPath: process.cwd(),
+          relativePath: '..',
+        },
+        depth: 2,
+        pos: {
+          line: 1,
+          column: 14,
+        },
+        identifier: {
+          name: 'toLocaleString',
+          alias: filenamify(uuid),
+        },
+        isAnonymous: false,
+        isPureType: false,
+        isDefault: false,
+        isIgnored: false,
+        comments: [],
+      } satisfies IExportStatement,
+    ]);
+  });
+
   it('one function, default export by anonymous', async () => {
     const uuid = randomUUID();
     const filename = `${uuid}.ts`;
