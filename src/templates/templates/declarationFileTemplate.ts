51f4a6febffe2c010c9eec8e96aea098e1366392
ByungJoon Lee <imjuni@gmail.com>
2024-08-19 02:02:55 +0900

    fix(#153): fixed the issue with @ctix-declaration statement generation
      - fixed the issue with @ctix-declaration statement generation
        - fixed the problem where the fileExt option was not working

--- src/templates/templates/declarationFileTemplate.ts
+++ src/templates/templates/declarationFileTemplate.ts
@@ -1,6 +1,6 @@
 export const declarationFileTemplate = `
 <%- it.declarations.forEach((declaration) => { -%>
-import <%-= it.options.quote %><%= declaration.relativePath %><%= it.options.quote -%>
+import <%-= it.options.quote %><%= declaration.relativePath %><%= declaration.extname.render %><%= it.options.quote -%>
 
 <%- }) %>
 `;
